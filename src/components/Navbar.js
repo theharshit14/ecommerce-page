3d295f0f33617750d9025d2d424966dd46745038
Harshit Verma <vharshit2verma@gmail.com>
2024-10-26 11:25:28 +0530

    code updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,20 +23,20 @@ const Navbar = () => {
             <div className="input-group ">
               <input
                 type="text"
-                className="form-control montserrat"
+                className="form-control montserrat mt-1"
                 placeholder="Search"
                 aria-label="Search"
                 aria-describedby="button-addon2"
               />
               <button
-                className="btn btn-outline-secondary"
+                className="btn btn-outline-secondary mt-1"
                 type="button"
                 id="button-addon2"
               >
                 <i className="bi bi-search"></i>
               </button>
             </div>
-            <a href="#" className="btn btn-link link-underline link-underline-opacity-0 montserrat"><i className="bi bi-person-circle"></i>Profile</a>
+            <a href="#" className="link-underline link-underline-opacity-0 montserrat"><i className="bi bi-person-circle"></i>Profile</a>
             <a href="#" className="btn btn-link link-underline link-underline-opacity-0 montserrat"><i className="bi bi-suit-heart-fill"></i>Wishlist</a>
             <a href="#" className="btn btn-link link-underline link-underline-opacity-0 montserrat"><i className="bi bi-bag-fill"></i>Cart</a>
           </div>
